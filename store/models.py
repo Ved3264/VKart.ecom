bd88d931123932ac204da42ff17062a8650898cc
ved patel <pved81034@gmail.com>
2024-07-16 18:33:03 +0530

    create store appp and create models for product and register model and then add product and apply on home page using vkart view file

---
+++ store/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from category.models import Category
+# Create your models here.
+class Product(models.Model):
+      product_name   =  models.CharField(max_length=200,unique=True)
+      slug           =  models.SlugField(max_length=200,unique=True)
+      discription    =  models.CharField(max_length=500,blank=True)
+      price          =  models.IntegerField()
+      stock          =  models.IntegerField()
+      image          =  models.ImageField(upload_to='photos/product')
+      is_available   =  models.BooleanField(default=True)
+      create_date    =  models.DateTimeField(auto_now_add=True)
+      update_date    =  models.DateTimeField(auto_now=True)
+      category       = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+      def __str__(self):
+            return self.product_name
+      
