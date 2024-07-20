9bf2e5923e1af275d35b4db1941398c21ab25ead
ved patel <pved81034@gmail.com>
2024-07-20 19:44:04 +0530

    edit stor.html page and view.html for display product in store page

--- store/views.py
+++ store/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 def store(request):
-      return render(request,'stores/store.html')
+      product_store  = Product.objects.all().filter(is_available=True)
+      product_count = product_store.count()
+
+      context={
+            'product_store':product_store,
+            'product_count':product_count,
+      }
+      return render(request,'stores/store.html',context)
