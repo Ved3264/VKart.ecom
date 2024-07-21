60a9d004c549879c52b52dbe4a80c241c31fa21c
ved patel <pved81034@gmail.com>
2024-07-21 19:06:22 +0530

    add product detaill template and method in store

--- store/views.py
+++ store/views.py
@@ -18,3 +18,7 @@ def store(request,category_slug=None):
             'product_count':product_count,
       }
       return render(request,'stores/store.html',context)
+
+
+def product_detail(request,category_slug,product_slug):
+      return render(request,'stores/product_detail.html')
