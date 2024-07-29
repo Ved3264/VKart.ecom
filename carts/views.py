a9572db0a933fa38a3a8faa50d3681310c7f8ee7
ved patel <pved81034@gmail.com>
2024-07-29 19:30:26 +0530

    add decrease and remove method

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from store.models import Product
 from .models import Cart,CartItem
 
@@ -10,13 +10,16 @@ def _cart_id(request):
 
 def add_cart(request,product_id):
       product = Product.objects.get(id=product_id)
+       #create sesion if not exist
       try:
             cart = Cart.objects.get(cart_id=_cart_id(request))
       except Cart.DoesNotExist:
             cart = Cart.objects.create(cart_id=_cart_id(request))
       cart.save()
 
+      #add product on cart item data
       try:
+            
             cart_item = CartItem.objects.get(product=product,cart=cart)
             cart_item.quantity += 1
             cart_item.save()
@@ -30,6 +33,24 @@ def add_cart(request,product_id):
       
       return redirect('cart')
 
+def remove_cart(request,product_id):
+      cart = Cart.objects.get(cart_id=_cart_id(request))
+      product = get_object_or_404(Product,id=product_id)
+      cart_item = CartItem.objects.get(product=product,cart=cart)
+      if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+      else:
+            cart_item.delete()
+      
+      return redirect('cart')
+
+def remove_cart_item(request,product_id):
+      cart = Cart.objects.get(cart_id=_cart_id(request))
+      product = get_object_or_404(Product,id=product_id)
+      cart_item = CartItem.objects.get(product=product,cart=cart)
+      cart_item.delete()
+      return redirect('cart')
 
 def cart(request,total=0,quantity=0,cart_items=None):
       try:
